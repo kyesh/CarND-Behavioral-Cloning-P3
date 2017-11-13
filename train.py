d13f0c3375ceb13f6fb459df7e4faf36e9f99084
unknown <kenyesh@gmail.com>
2017-11-12 19:51:49 -0500

    First time around the track!

--- train.py
+++ train.py
@@ -4,6 +4,28 @@ import numpy as np
 
 correction = 0.2
 
+
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers.convolutional import Conv2D
+
+model = Sequential()
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Conv2D(24, (5,5), activation="relu", strides=(2,2)))
+model.add(Conv2D(36, (5,5), strides=(2,2), activation="relu"))
+model.add(Conv2D(48,(5,5), strides=(2,2), activation="relu"))
+model.add(Conv2D(64,(3,3), strides=(2,2), activation="relu"))
+#model.add(Conv2D(64,(3,3), strides=(2,2), activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse',optimizer='adam')
+
 lines = []
 with open('data/data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
@@ -52,16 +74,6 @@ for image,measurment in zip(images,measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
-
-model = Sequential()
-model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-model.add(Flatten())
-model.add(Dense(1))
-
-model.compile(loss='mse',optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
 
 model.save('model.h5')
